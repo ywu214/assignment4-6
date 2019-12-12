8e61b8beaed883d3277c00deba1b750cc4c52fdf
xiaohang.liu <xiaohang.liu@luckincoffee.com>
2019-12-12 11:30:30 +0800

    fix

--- routes/index.js
+++ routes/index.js
@@ -2,10 +2,12 @@ const express = require('express');
 const router = express.Router();
 
 router.get('/', function(req, res, next) {
-  res.render('index', {
-    user: req.session.user,
-    loggedIn: req.session.loggedIn,
-    isAdmin:req.session.admin });
+  // res.render('index', {
+  //   user: req.session.user,
+  //   loggedIn: req.session.loggedIn,
+  //   isAdmin:req.session.admin });
+
+  res.render('index');
 });
 
 router.get('/login', function(req, res, next) {
